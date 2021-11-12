1a09bba74032b2893533f1fb62c7a29f70f16bba
jaybell <jaycooperbell@gmail.com>
2021-11-11 21:57:28 -0800

    upgrade repo to nx 13

--- apps/app0/src/app/app.component.spec.ts
+++ apps/app0/src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Lib0Module } from '@largerepo/app0/lib0/lib0';
+import { Lib1Module } from '@largerepo/app0/lib1/lib1';
+import { Lib2Module } from '@largerepo/app0/lib2/lib2';
+import { Lib3Module } from '@largerepo/app0/lib3/lib3';
+import { Lib4Module } from '@largerepo/app0/lib4/lib4';
 
 describe('AppComponent', () => {
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [AppComponent],
+        imports: [Lib0Module, Lib1Module, Lib2Module, Lib3Module, Lib4Module],
+      }).compileComponents();
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
